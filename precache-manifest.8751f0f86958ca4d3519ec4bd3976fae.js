c3ab07e28f645e24645a551df35e5768d1fd58f5
Cheryl Liao <cheryl@Cheryls-MacBook-Air.local>
2020-02-13 22:05:47 -0300

    Updates

---
+++ precache-manifest.8751f0f86958ca4d3519ec4bd3976fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de57bade848c32862db4885ea07c48cc",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "ebfd4a42a4ba328f08ee",
+    "url": "/monsters-search/static/css/main.8fc6ca98.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/monsters-search/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-search/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfd4a42a4ba328f08ee",
+    "url": "/monsters-search/static/js/main.d11cce02.chunk.js"
+  },
+  {
+    "revision": "8dbc919f0c5c5ce1bcb8",
+    "url": "/monsters-search/static/js/runtime-main.e5af9170.js"
+  }
+]);
